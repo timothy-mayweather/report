bd56268e4e81c55b66f0b5110559f912f194116e
Timothy Kibalama <timkibalama@gmail.com>
2023-08-21 02:54:42 +0300

    added sample user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+	    DB::table('users')->insert([
+		    'name' => 'Timothy Kibalama',
+		    'email' => 'timkibalama@gmail.com',
+		    'password' => '$2y$10$EJMQNG4O6Nr8EEu.SU1yjeRKl.71Orm7W1zsrVY0tn5AqPjWBGSCy',
+		    'role' => 'employee'
+	    ]);
     }
 }
