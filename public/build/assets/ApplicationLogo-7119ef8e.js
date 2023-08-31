024487b4781f8892139f4b1ebf01911e7e96eaae
Timothy Kibalama <timkibalama@gmail.com>
2023-08-31 06:22:02 +0300

    changed app name

---
+++ public/build/assets/ApplicationLogo-7119ef8e.js
@@ -0,0 +1,1 @@
+import{j as s,a as w}from"./app-106109f1.js";function g(t){return s("svg",{viewBox:"0 0 316 316",xmlns:"http://www.w3.org/2000/svg",width:t.width,children:['xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg"',w("image",{width:"100%",height:"100%",href:"/logo.png",x:"0",y:"0",id:"image10",preserveAspectRatio:"none"})]})}export{g as A};
